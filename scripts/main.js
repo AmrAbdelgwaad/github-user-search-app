ca9293becf48ffd4bd66bbe0918e7f3884e839cf
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2023-10-30 06:00:50 +0200

    Adding theme toggle + some progress with layout

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+/*
+const url = "https://api.github.com/users";
+const headers = {
+  Authorization: "Your Auth",
+};
+const body = {
+  name: "New User",
+  email: "newuser@example.com",
+  password: "password",
+};
+
+fetch(url, {
+  method: "POST",
+  headers: headers,
+  body: JSON.stringify(body),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+
+*/
---
+++ scripts/theme.js
@@ -0,0 +1,54 @@
+const storageKey = "theme-preference";
+
+let audio = new Audio("click.wav");
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === "light" ? "dark" : "light";
+  audio.play().then(() => audio.play());
+  setPreference();
+};
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+};
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme.value);
+
+  let themeName = document.getElementById("theme-name");
+  if (themeName !== null) {
+    themeName.innerHTML = theme.value;
+  }
+};
+
+const theme = {
+  value: getColorPreference(),
+};
+
+// set early so no page flashes / CSS is made aware
+reflectPreference();
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference();
+
+  // now this script can find and listen for clicks on the control
+  document.querySelector("#theme-toggle").addEventListener("click", onClick);
+};
+
+// sync with system changes
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    theme.value = isDark ? "dark" : "light";
+    setPreference();
+  });
